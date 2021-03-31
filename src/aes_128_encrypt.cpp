dd3e2835bcbdd80b6b5dd0cc12ab7abd3da1a65c
Dbrownell623 <dbrownell623@gmail.com>
2021-03-31 15:51:35 -0400

    update

--- src/aes_128_encrypt.cpp
+++ src/aes_128_encrypt.cpp
@@ -35,7 +35,31 @@ void shift_rows(byte *byte_array) {
 }
 
 // Mix Columns
-void mix_columns(byte *byte_array) {
-    // Fill this function
+void mix_columns(byte *array) {
+    byte tmp[16];
+
+    tmp[0] = (byte)(mul2[array[0]] ^ mul3[array[1]] ^ array[2] ^ array[3]);
+    tmp[1] = (byte)(array[0] ^ mul2[array[1]] ^ mul3[array[2]] ^ array[3]);
+    tmp[2] = (byte)(array[0] ^ array[1] ^ mul2[array[2]] ^ mul3[array[3]]);
+    tmp[3] = (byte)(mul3[array[0]] ^ array[1] ^ array[2] ^ mul2[array[3]]);
+
+    tmp[4] = (byte)(mul2[array[4]] ^ mul3[array[5]] ^ array[6] ^ array[7]);
+    tmp[5] = (byte)(array[4] ^ mul2[array[5]] ^ mul3[array[6]] ^ array[7]);
+    tmp[6] = (byte)(array[4] ^ array[5] ^ mul2[array[6]] ^ mul3[array[7]]);
+    tmp[7] = (byte)(mul3[array[4]] ^ array[5] ^ array[6] ^ mul2[array[7]]);
+
+    tmp[8] = (byte)(mul2[array[8]] ^ mul3[array[9]] ^ array[10] ^ array[11]);
+    tmp[9] = (byte)(array[8] ^ mul2[array[9]] ^ mul3[array[10]] ^ array[11]);
+    tmp[10] = (byte)(array[8] ^ array[9] ^ mul2[array[10]] ^ mul3[array[11]);
+    tmp[11] = (byte)(mul3[array[8]] ^ array[9] ^ array[10] ^ mul2[array[11]]);
+
+    tmp[12] = (byte)(mul2[array[12]] ^ mul3[array[13]] ^ array[14] ^ array[15]);
+    tmp[13] = (byte)(array[12] ^ mul2[array[13]] ^ mul3[array[14]] ^ array[15]);
+    tmp[14] = (byte)(array[12] ^ array[13] ^ mul2[array[14]] ^ mul3[array[15]]);
+    tmp[15] = (byte)(mul3[array[12]] ^ array[13] ^ array[14] ^ mul2[array[15]]);
+
+    for (int i = 0; i < 16; i++){
+        array[i] = tmp[i];
+    }
 }
 
